d1b0e891f07e60723052fcffcfea2d5a2e8394d8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-17 23:01:02 +0600

    add footer and implement

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 import logo from "../../../assets/logo.svg"
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content mt-20">
+        <footer className="footer p-10 bg-base-200 text-base-content">
             <aside>
                <img src={logo} ></img>
                 <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
