c9c0976a638c3cdc79b85d747fcc133c87f89bf8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-17 23:00:38 +0600

    add footer and implement

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -8,7 +8,11 @@ const Footer = () => {
                 <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
             </aside>
             <nav>
-                
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
             </nav>
             <nav>
                 <h6 className="footer-title">Company</h6>
