5dac24de5e648f1498597d2daf075ec46919c3eb
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-17 23:00:07 +0600

    add checkout

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -2,17 +2,13 @@
 import logo from "../../../assets/logo.svg"
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
+        <footer className="footer p-10 bg-base-200 text-base-content mt-20">
             <aside>
                <img src={logo} ></img>
                 <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
             </aside>
             <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+                
             </nav>
             <nav>
                 <h6 className="footer-title">Company</h6>
