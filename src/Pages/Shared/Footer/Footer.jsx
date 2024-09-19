179c4a556a6cb59c80fcd21d872671cf076c4824
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-19 21:27:07 +0600

    to do some

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -15,7 +15,11 @@ const Footer = () => {
                 <a className="link link-hover">Advertisement</a>
             </nav>
             <nav>
-               
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
             </nav>
             <nav>
                 <h6 className="footer-title">Legal</h6>
