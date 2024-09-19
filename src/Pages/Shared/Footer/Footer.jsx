59cfb3907d7b142486167310a7b5a42655b06e1b
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-19 21:25:27 +0600

    delete something

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -22,7 +22,10 @@ const Footer = () => {
                 <a className="link link-hover">Press kit</a>
             </nav>
             <nav>
-               
+                <h6 className="footer-title">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
             </nav>
         </footer>
     );
